a425457f415a4bd9c9fdbaa52ccd533614929aa0
Aiswarya Rajendran <Aiswarya Rajendran@192.168.1.35>
2026-01-05 20:59:32 +0530

    Added excel read functionality

---
+++ src/main/java/excel/read/ExcelRead.java
@@ -0,0 +1,80 @@
+package excel.read;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.apache.poi.ss.usermodel.*;
+
+public class ExcelRead {
+	public static XSSFWorkbook w;
+	public static XSSFSheet s;
+	public static FileInputStream f;
+
+	public static String readStringData(int i,int j) throws IOException {
+
+	f= new FileInputStream("Student.xlsx");
+
+
+
+	w= new XSSFWorkbook(f);
+
+
+
+	s= w.getSheet("Sheet1");
+
+	Row r=s.getRow(i);
+
+
+
+	Cell c=r.getCell(j);
+
+
+
+	return c.getStringCellValue();
+
+
+
+	}
+
+
+
+	public static String readIntegerData(int i,int j) throws IOException {
+
+		
+
+
+
+			f= new FileInputStream("C:\\Users\\Aiswarya Rajendran\\eclipse-workspace\\Excelread\\src\\main\\resources\\Student.xlsx");
+
+
+
+			w= new XSSFWorkbook(f);
+
+
+
+			s= w.getSheet("Sheet1");
+
+
+
+			Row r=s.getRow(i);
+
+
+
+			Cell c=r.getCell(j);
+
+
+
+			int value=(int) c.getNumericCellValue();
+
+
+
+			return String.valueOf(value);
+
+
+
+			}
+
+	}
